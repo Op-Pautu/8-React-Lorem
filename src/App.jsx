00693a191108429685697fe03e5e8f34540b2e1d
Op Pautu <pautuop@gmail.com>
2023-03-05 18:29:05 +0530

    Add api instead of static data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
-import data from "./data";
+
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
-  const handleSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     let amount = parseInt(count);
     if (count <= 0) {
       amount = 1;
     }
-    if (count > 8) {
-      amount = 8;
+    if (count > 50) {
+      amount = 50;
+      alert("Maximum limit exceeded. Generating 50 paragraphs.");
     }
 
-    setText(data.slice(0, amount));
+    setLoading(true);
+
+    const apiUrl = `https://hipsum.co/api/?type=hipster-centric&paras=${amount}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setText(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
@@ -27,16 +43,14 @@ function App() {
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button type="submit" className="btn">
-          generate
+        <button type="submit" className="btn" disabled={loading}>
+          {loading ? "Generating..." : "Generate"}
         </button>
       </form>
       <article className="lorem-text">
-        <p>
-          {text.map((item, index) => {
-            return <p key={index}>{item}</p>;
-          })}
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
